047966f18a63daed5d108d962818bfc84aedd404
Puru Lokendra Singh <purulokendra@gmail.com>
2022-10-09 16:58:05 -0700

    Time: 175 ms (34.37%), Space: 15.2 MB (25.14%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print('TESTING LEETHUB EXTENSION')
         ddict = {}
         for i in range(len(nums)):
             if (target - nums[i]) in ddict:

0c120f9fdd65df47062605d923ce535d297ef67a
Puru Lokendra Singh <purulokendra@gmail.com>
2022-10-21 21:07:19 -0700

    Time: 86 ms (44.85%), Space: 15 MB (38.39%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,25 +1,25 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         dp = defaultdict(lambda: None)
-        len_s1, len_s2, len_s3 = len(s1), len(s2), len(s3)
-        
-        if len_s1 + len_s2 != len_s3:
+        m, n = len(s1), len(s2)
+
+        if m + n != len(s3):
             return False
-        
-        def helper(p1, p2):
-            if dp[p1, p2] != None:
-                return dp[p1, p2]
-            if p1 == -1:
-                dp[p1, p2] = (s2[:p2+1] == s3[:p2+1])
-            elif p2 == -1:
-                dp[p1, p2] = (s1[:p1+1] == s3[:p1+1])
+
+        def helper(i, j):
+            if dp[i, j] != None:
+                return dp[i, j]
+            if i < 0:
+                dp[i, j] = (s2[:j+1] == s3[:j+1])
+            elif j < 0:
+                dp[i, j] = (s1[:i+1] == s3[:i+1])
             else:
                 t = []
-                if s1[p1] == s3[p1+p2+1]:
-                    t.append(helper(p1-1, p2))
-                if s2[p2] == s3[p1+p2+1]:
-                    t.append(helper(p1, p2-1))
-                dp[p1, p2] = any(t) if t else False
-            return dp[p1, p2]
-        
-        return helper(len_s1-1, len_s2-1)
+                if s1[i] == s3[i+j+1]:
+                    t.append(helper(i-1, j))
+                if s2[j] == s3[i+j+1]:
+                    t.append(helper(i, j-1))
+                dp[i, j] = any(t) if t else False
+            return dp[i, j]
+
+        return helper(m-1, n-1)

df38219736a2772093b687b5059920b29de55da7
Puru Lokendra Singh <purulokendra@gmail.com>
2022-10-21 19:47:39 -0700

    Time: 49 ms (89.02%), Space: 14.9 MB (38.39%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -6,20 +6,20 @@ class Solution:
         if len_s1 + len_s2 != len_s3:
             return False
         
-        def helper(p1, p2, p3):
+        def helper(p1, p2):
             if dp[p1, p2] != None:
                 return dp[p1, p2]
             if p1 == -1:
-                dp[p1, p2] = (s2[:p2+1] == s3[:p3+1])
+                dp[p1, p2] = (s2[:p2+1] == s3[:p2+1])
             elif p2 == -1:
-                dp[p1, p2] = (s1[:p1+1] == s3[:p3+1])
+                dp[p1, p2] = (s1[:p1+1] == s3[:p1+1])
             else:
                 t = []
-                if s1[p1] == s3[p3]:
-                    t.append(helper(p1-1, p2, p3-1))
-                if s2[p2] == s3[p3]:
-                    t.append(helper(p1, p2-1, p3-1))
+                if s1[p1] == s3[p1+p2+1]:
+                    t.append(helper(p1-1, p2))
+                if s2[p2] == s3[p1+p2+1]:
+                    t.append(helper(p1, p2-1))
                 dp[p1, p2] = any(t) if t else False
             return dp[p1, p2]
         
-        return helper(len_s1-1, len_s2-1, len_s3-1)
+        return helper(len_s1-1, len_s2-1)

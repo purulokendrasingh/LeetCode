0baa7a296476a7f0a5c0b9e8154bd7b4560fef1a
Puru Lokendra Singh <purulokendra@gmail.com>
2023-01-03 23:12:54 -0700

    Time: 3120 ms (5.91%), Space: 72.8 MB (5.08%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        counts = Counter(tasks).values()
+        if 1 in counts:
+            return -1
+        
+        dp = defaultdict(lambda: None)
+        
+        def helper(n):
+            if dp[n] != None:
+                return dp[n]
+            if n == 0:
+                dp[n] = 0
+            elif n < 0:
+                dp[n] = 100000
+            else:
+                dp[n] = min(1 + helper(n-3), 1 + helper(n-2))
+            return dp[n]
+        
+        ans = 0
+        for i in counts:
+            ans += helper(i)
+        return ans

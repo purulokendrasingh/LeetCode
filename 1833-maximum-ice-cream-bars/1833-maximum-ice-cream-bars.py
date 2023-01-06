1a500f1400fd6f3e4576377d320dfd314880a6ec
Puru Lokendra Singh <purulokendra@gmail.com>
2023-01-05 19:08:42 -0700

    Time: 2376 ms (10.47%), Space: 28 MB (61.26%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -2,8 +2,10 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         costs.sort()
         costs = list(accumulate(costs))
+        ans = 0
         if coins in costs:
-            return costs.index(coins) + 1
+            ans = costs.index(coins) + 1
         else:
             bisect.insort(costs, coins)
-            return costs.index(coins)
+            ans = costs.index(coins)
+        return ans

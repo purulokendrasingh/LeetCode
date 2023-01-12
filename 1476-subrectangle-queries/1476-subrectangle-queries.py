4c49010fff0de3f7e58f9ed689269a8bb03b82c0
Puru Lokendra Singh <purulokendra@gmail.com>
2023-01-12 01:08:39 -0700

    Time: 150 ms (97.98%), Space: 16 MB (60.50%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.py
+++ 1476-subrectangle-queries/1476-subrectangle-queries.py
@@ -4,9 +4,9 @@ class SubrectangleQueries:
         self.rect = rectangle
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        temp = [newValue]*(col2-col1+1)
         for i in range(row1, row2+1):
-            for j in range(col1, col2+1):
-                self.rect[i][j] = newValue
+            self.rect[i][col1:col2+1] = temp
 
     def getValue(self, row: int, col: int) -> int:
         return self.rect[row][col]

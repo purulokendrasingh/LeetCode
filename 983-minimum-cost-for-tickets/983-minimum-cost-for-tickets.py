ef1e9de2a94c3fa84674f9c03cb6fcdc04f6f72a
Puru Lokendra Singh <purulokendra@gmail.com>
2023-03-28 01:26:35 -0700

    Time: 54 ms (37.30%), Space: 14.5 MB (27.10%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,24 +1,27 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         n = len(days)
+        dp = defaultdict(lambda: None)
         
-        @lru_cache(1000)
         def helper(idx):
+            if dp[idx] != None:
+                return dp[idx]
             if idx >= n:
-                return 0
-            
-            a = costs[0] + helper(idx + 1)
-            
-            i = idx
-            while i < n and days[i] <= days[idx] + 6:
-                i += 1
-            b = costs[1] + helper(i)
-            
-            j = idx
-            while j < n and days[j] <= days[idx] + 29:
-                j += 1
-            c = costs[2] + helper(j)
-            
-            return min(a,b,c)
+                dp[idx] = 0
+            else:
+                a = costs[0] + helper(idx + 1)
+
+                i = idx
+                while i < n and days[i] <= days[idx] + 6:
+                    i += 1
+                b = costs[1] + helper(i)
+
+                j = idx
+                while j < n and days[j] <= days[idx] + 29:
+                    j += 1
+                c = costs[2] + helper(j)
+                
+                dp[idx] = min(a,b,c)
+            return dp[idx]
         
         return helper(0)

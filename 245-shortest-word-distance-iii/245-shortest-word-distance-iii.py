1bdd17d84febbaaa5c21693495d7885da256e8a3
Puru Lokendra Singh <purulokendra@gmail.com>
2023-04-05 07:29:16 -0700

    Time: 741 ms (85.73%), Space: 37 MB (94.96%) - LeetHub

--- 245-shortest-word-distance-iii/245-shortest-word-distance-iii.py
+++ 245-shortest-word-distance-iii/245-shortest-word-distance-iii.py
@@ -10,29 +10,42 @@ class Solution:
             if wordsDict[i] != word1 and wordsDict[i] != word2:
                 continue
                 
-            if rec == 1:
-                if wordsDict[i] == word2:
-                    w2 = i
-                    ans = min(ans, abs(w1-w2))
-                    rec = 2
-                else:
-                    w1 = i
-            elif rec == 2:
-                if wordsDict[i] == word1:
-                    w1 = i
-                    ans = min(ans, abs(w1-w2))
-                    rec = 1
-                else:
-                    w2 = i
-            else:
-                if wordsDict[i] == word1:
-                    w1 = i
-                    ans = min(ans, abs(w1-w2))
-                    rec = 1
-                elif wordsDict[i] == word2:
-                    w2 = i
-                    ans = min(ans, abs(w1-w2))
-                    rec = 2
+            if wordsDict[i] == word1 and rec != 1:
+                w1 = i
+                ans = min(ans, abs(w1-w2))
+                rec = 1
+            elif wordsDict[i] == word2 and rec != 2:
+                w2 = i
+                ans = min(ans, abs(w1-w2))
+                rec = 2
+            elif wordsDict[i] == word1 and rec == 1:
+                w1 = i
+            elif wordsDict[i] == word2 and rec == 2:
+                w2 = i
+                
+            # if rec == 1:
+            #     if wordsDict[i] == word2:
+            #         w2 = i
+            #         ans = min(ans, abs(w1-w2))
+            #         rec = 2
+            #     else:
+            #         w1 = i
+            # elif rec == 2:
+            #     if wordsDict[i] == word1:
+            #         w1 = i
+            #         ans = min(ans, abs(w1-w2))
+            #         rec = 1
+            #     else:
+            #         w2 = i
+            # else:
+            #     if wordsDict[i] == word1:
+            #         w1 = i
+            #         ans = min(ans, abs(w1-w2))
+            #         rec = 1
+            #     elif wordsDict[i] == word2:
+            #         w2 = i
+            #         ans = min(ans, abs(w1-w2))
+            #         rec = 2
         return ans
                 
             

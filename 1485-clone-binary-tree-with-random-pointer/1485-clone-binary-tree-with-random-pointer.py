fec65f3dc3c5186b9881a70c43448a832e039ab4
Puru Lokendra Singh <purulokendra@gmail.com>
2023-04-05 08:27:10 -0700

    Time: 275 ms (5.07%), Space: 19.8 MB (14.42%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -6,6 +6,8 @@
 #         self.right = right
 #         self.random = random
 
+import ctypes
+
 class Solution:
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
         otn = {}
@@ -13,12 +15,11 @@ class Solution:
         def preorder(head):
             node = None
             if head:
-                if head not in otn:
+                if id(head) not in otn:
                     node = NodeCopy(head.val)
-                    otn[head] = node
+                    otn[id(head)] = id(node)
                 else:
-                    node = otn[head]
-                    return node
+                    return ctypes.cast(otn[id(head)], ctypes.py_object).value
                 node.random = preorder(head.random)
                 node.left = preorder(head.left)
                 node.right = preorder(head.right)

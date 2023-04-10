6752221e173f8f301a402971575a7c8dbbe7a684
Puru Lokendra Singh <purulokendra@gmail.com>
2023-04-10 03:32:43 -0700

    Time: 24 ms (96.11%), Space: 14 MB (16.28%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,15 +5,15 @@ class Solution:
             '}':'{',
             ']':'['
         }
-        q = deque()
+        q = []
         for i in s:
             if i in ('(','{','['):
-                q.appendleft(i)
+                q.append(i)
             else:
                 if not q:
                     return False
-                if mapp[i] == q[0]:
-                    q.popleft()
+                if mapp[i] == q[-1]:
+                    q.pop()
                 else:
                     return False
         return True if not q else False

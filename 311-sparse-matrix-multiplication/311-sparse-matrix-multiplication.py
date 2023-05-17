34caa11940d0dedcff8636c2e81e53f5d03e55d2
Puru Lokendra Singh <purulokendra@gmail.com>
2023-05-17 09:30:31 -0700

    Time: 161 ms (25.13%), Space: 16.5 MB (72.47%) - LeetHub

---
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -0,0 +1,16 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
+        
+        matt = []
+        for _ in range(m):
+            matt.append([0]*n)
+        
+        for i in range(m):
+            for j in range(n):
+                temp = 0
+                for l in range(k):
+                    temp += mat1[i][l]*mat2[l][j]
+                matt[i][j] = temp
+        
+        return matt

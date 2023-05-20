296fc131448091b452c284d3b1ac5cc9deb1e8db
Puru Lokendra Singh <purulokendra@gmail.com>
2023-05-19 18:28:19 -0700

    Time: 52 ms (12.53%), Space: 16.4 MB (25.75%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.py
@@ -0,0 +1,36 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        n = len(equations)
+
+        for i in range(n):
+            graph[equations[i][0]].append((equations[i][1], values[i]))
+            graph[equations[i][1]].append((equations[i][0], 1/values[i]))
+            
+        vertices = list(graph.keys())
+            
+        def bfs(sourceNode, targetNode):
+            if sourceNode not in vertices or targetNode not in vertices:
+                return -1
+            # elif sourceNode == targetNode:
+            #     return 1
+            
+            vis = defaultdict(bool)
+            q = deque([(sourceNode, 1)])
+            vis[sourceNode] = True
+            while q:
+                count = len(q)
+                for _ in range(count):
+                    u, mult = q.popleft()
+                    if u == targetNode:
+                        return mult
+                    for v, edge in graph[u]:
+                        if not vis[v]:
+                            vis[v] = True
+                            q.append((v, mult*edge))
+            return -1
+        
+        ans = []
+        for s,t in queries:
+            ans.append(bfs(s,t))
+        return ans

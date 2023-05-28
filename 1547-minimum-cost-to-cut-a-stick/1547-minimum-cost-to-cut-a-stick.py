3f24c0cc09c7b2e3c3b3dc03d667e26d582b0aef
Puru Lokendra Singh <purulokendra@gmail.com>
2023-05-28 16:27:22 -0700

    Time: 3273 ms (5.18%), Space: 20.2 MB (16.77%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        dp = defaultdict(lambda: None)
+        
+        def helper(l,r, cuts):
+            if dp[l,r] != None:
+                return dp[l,r]
+            if not cuts:
+                dp[l,r] = 0
+            else:
+                temp = float('inf')
+                cPass = False
+                for c in cuts:
+                    if c < r and c > l:
+                        if not cPass:
+                            cPass = True
+                        a = cuts[::]
+                        a.remove(c)
+                        temp = min(temp, helper(l, c, a) + helper(c, r, a) + r-l)
+                if cPass:
+                    dp[l,r] = temp
+                else:
+                    dp[l,r] = 0
+            return dp[l,r]
+        
+        cuts = sorted(cuts)
+        
+        return helper(0, n, cuts)

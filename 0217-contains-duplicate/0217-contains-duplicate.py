1328febe498abe57f951fc04ab6bba8bc4d8d04e
Puru Lokendra Singh <purulokendra@gmail.com>
2023-08-21 10:44:56 -0700

    Time: 485 ms (74.41%), Space: 33.4 MB (22.40%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        vis = defaultdict(bool)
+        for num in nums:
+            if vis[num]:
+                return True
+            vis[num] = True
+        return False

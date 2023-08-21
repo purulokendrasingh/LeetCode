7bca19a8f92d8d13451184b89033058473610738
Puru Lokendra Singh <purulokendra@gmail.com>
2023-08-21 10:59:04 -0700

    Time: 49 ms (90.44%), Space: 16.9 MB (62.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # APPROACH 1
+        '''
         if len(s) != len(t):
             return False
         
@@ -9,4 +11,17 @@ class Solution:
             s_counter[i] += 1
             t_counter[j] += 1
         
+        return s_counter == t_counter
+        '''
+        
+        # APPROACH 2
+        '''
+        return sorted(s) == sorted(t)
+        '''
+        
+        # APPROACH 3
+        if len(s) != len(t):
+            return False
+        
+        s_counter, t_counter = Counter(s), Counter(t)
         return s_counter == t_counter

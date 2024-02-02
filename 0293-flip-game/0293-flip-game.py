a05e5670797f0e69be83d83b8842244f6bf55e31
Puru Lokendra Singh <purulokendra@gmail.com>
2024-02-02 12:25:00 -0700

    Time: 42 ms (43.30%), Space: 17 MB (64.37%) - LeetHub

---
+++ 0293-flip-game/0293-flip-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        ans = []
+        n = len(currentState)
+        if n < 2 or currentState.count('+') == 0:
+            return []
+        pointer = 0
+        while pointer < n-1:
+            if currentState[pointer:pointer+2] == '++':
+                ans.append(currentState[:pointer]+'--'+currentState[pointer+2:])
+            # elif currentState[pointer:pointer+2] == '--':
+            #     t = currentState[:pointer]+'++'+currentState[pointer+2:]
+            #     ans.append(t)
+            pointer += 1
+        return ans

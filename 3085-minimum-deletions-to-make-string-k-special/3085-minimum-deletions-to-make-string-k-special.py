1696570725f64c577125fa34c0755deba535765b
Puru Lokendra Singh <purulokendra@gmail.com>
2024-03-17 20:57:04 -0700

    Time: 99 ms (100.00%), Space: 17.5 MB (100.00%) - LeetHub

---
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        c = [v for k,v in Counter(word).most_common()][::-1]
+        n = len(c)
+        ans = 100000
+        for i in range(n):
+            temp = sum(c[:i])
+            for j in range(i+1, n):
+                if abs(c[j] - c[i]) > k:
+                    temp += (c[j]-c[i]-k)
+            
+            ans = min(ans, temp)
+        return ans

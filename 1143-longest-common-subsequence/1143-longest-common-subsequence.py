9045da413ca4626d91bcae2a2e06300c944885f0
Puru Lokendra Singh <purulokendra@gmail.com>
2024-04-09 14:03:37 -0700

    Time: 1501 ms (5.01%), Space: 308.8 MB (12.19%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,17 +2,16 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = defaultdict(lambda: None)
         
-        def helper(p1, p2):
-            if dp[p1, p2] != None:
-                return dp[p1,p2]
-            if p1 < 0 or p2 < 0:
-                dp[p1, p2] = 0
-                return dp[p1, p2]
+        def helper(i, j):
+            if dp[i,j] is not None:
+                return dp[i,j]
+            if i < 0 or j < 0:
+                dp[i,j] = 0
             else:
-                if text1[p1] == text2[p2]:
-                    dp[p1,p2] = 1 + helper(p1-1, p2-1)
+                if text1[i] == text2[j]:
+                    dp[i,j] = 1 + helper(i-1,j-1)
                 else:
-                    dp[p1, p2] = max(helper(p1, p2-1), helper(p1-1, p2))
-            return dp[p1,p2]
+                    dp[i,j] = max(helper(i, j-1), helper(i-1, j))
+            return dp[i,j]
         
         return helper(len(text1)-1, len(text2)-1)

8a22bfe602b7ca6692f531b27a5b3f63ffe1784b
Puru Lokendra Singh <purulokendra@gmail.com>
2024-06-19 09:50:21 -0700

    Time: 50 ms (82.41%), Space: 17 MB (93.53%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.py
+++ 0422-valid-word-square/0422-valid-word-square.py
@@ -1,10 +1,5 @@
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
-        # for i in range(len(words)):
-        #     for j in range(len(words[0])):
-        #         if (j >= len(words[i]) and i < len(words[j])) or (j < len(words[i]) and i >= len(words[j])):
-        #             return False
-        #         if words[]
         maxLen = max([len(i) for i in words])
         if len(words) != maxLen or len(words[0]) != maxLen:
             return False
@@ -13,7 +8,6 @@ class Solution:
         for i in range(m):
             if len(words[i]) < n:
                 words[i] += '0'*(n-len(words[i]))
-        print(words)
         for i in range(m):
             for j in range(n):
                 if words[i][j] != words[j][i]:
